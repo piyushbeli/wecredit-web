660c152b6512b9525a7563b200d5e0211be8499a
Saurabh Shukla <saurabhshukla3107@gmail.com>
2026-06-02 18:59:21 +0530

    Remove revalidation settings from blog route fetch function

--- lib/sitemap/fetch-blog-routes-from-sheet.ts
+++ lib/sitemap/fetch-blog-routes-from-sheet.ts
@@ -5,7 +5,6 @@
 
 const DEFAULT_SHEET_ID = '1-EPYlYvCImOcqBSamBG1f5ak_-xaSM-Ddk8wv6QHkiU';
 const DEFAULT_BLOG_GID = '1704517346';
-const SHEET_REVALIDATE_SECONDS = 60; // every 1 minute
 
 export interface BlogRouteMapping {
   destination: string;
@@ -96,8 +95,6 @@ function parseBlogRoutesFromCsv(csvText: string): BlogRouteMapping[] {
 export async function fetchBlogRoutesFromSheet(): Promise<BlogRouteMapping[]> {
   try {
     const response = await fetch(getSheetExportUrl(), {
-      next: { revalidate: SHEET_REVALIDATE_SECONDS },
-      // no cache
       cache: 'no-store',
     });
 
