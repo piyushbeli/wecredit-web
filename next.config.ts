04ddbc6a642adc17096e18534ebb1ec6c1b021d1
Saurabh Shukla <saurabhshukla3107@gmail.com>
2026-06-12 14:37:12 +0530

    Remove outdated blog URL redirects from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,16 +9,6 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/blog',
-        destination: 'https://blog.wecredit.co.in/blog',
-        permanent: true,
-      },
-      {
-        source: '/blogs',
-        destination: 'https://blog.wecredit.co.in/blog',
-        permanent: true,
-      },
-      {
         source: '/terms',
         destination: 'https://www.wecredit.co.in/terms-of-service',
         permanent: true,
